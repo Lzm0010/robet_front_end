1b6075c90881b6fadae0a02a3232cbc16228dc36
Lee McCormick <lzm0010@gmail.com>
2020-04-23 18:01:00 -0500

    added reactcard flip

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,12 +1,28 @@
-import React, {Fragment} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import ReactCardFlip from 'react-card-flip';
 import Login from '../components/login';
 import Signup from '../components/signup';
 
 export default function Auth () {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlipClick = useCallback(e => {
+        e.preventDefault();
+        setIsFlipped(!isFlipped);
+    }, [isFlipped]);
+
+    useEffect(() => {
+       document.addEventListener('click', handleFlipClick);
+
+       return () => {
+        document.removeEventListener('click', handleFlipClick);
+       };
+    }, [handleFlipClick]);
+
     return (
-        <Fragment>
-            <Login />
-            <Signup />
-        </Fragment>
+        <ReactCardFlip isFlipped={isFlipped}>
+            <Login flip={handleFlipClick} flipDirection="horizontal" flipSpeedBackToFront={0.1}/>
+            <Signup flip={handleFlipClick} flipDirection="horizontal" flipSpeedBackToFront={0.1}/>
+        </ReactCardFlip>
     );
 }
